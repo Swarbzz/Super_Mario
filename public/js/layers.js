d7fd769f740acd0d25c510cc840b1c9432081d0c
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-06-04 16:04:51 +0100

    moved layer function into file

---
+++ public/js/layers.js
@@ -0,0 +1,22 @@
+function drawBackground(background, context, sprites) {
+  background.ranges.forEach(([x1, x2, y1, y2]) => {
+    for (let x = x1; x < x2; ++x) { //width of the sky
+      for (let y = y1; y < y2; ++ y) { // length of the sky
+        sprites.drawTile(background.tile, context, x, y); //this line determines where it should be displayed on the page
+      }
+    }  
+  });
+}
+
+export function createBackgroundLayer(backgrounds, sprites) {
+  const buffer = document.createElement('canvas');
+  buffer.width = 256;
+  buffer.height = 240;
+  
+  backgrounds.forEach(background => {
+    drawBackground(background, buffer.getContext('2d'), sprites); //selects the tiles in 1-1.json via the arrays and displays them
+  });
+  return function drawBackgroundLayer(context) {
+    context.drawImage(buffer, 0, 0);
+  };
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,10 @@
-import Compositor from './compsitor.js'
+import Compositor from './compsitor.js';
 import {loadLevel} from './Loaders.js';
 import {loadBackgroundSprites, loadMarioSprite} from './sprites.js';
+import {createBackgroundLayer} from './layers.js';
 
-function drawBackground(background, context, sprites) {
-  background.ranges.forEach(([x1, x2, y1, y2]) => {
-    for (let x = x1; x < x2; ++x) { //width of the sky
-      for (let y = y1; y < y2; ++ y) { // length of the sky
-        sprites.drawTile(background.tile, context, x, y); //this line determines where it should be displayed on the page
-      }
-    }  
-  });
-}
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
-
-function createBackgroundLayer(backgrounds, sprites) {
-  const buffer = document.createElement('canvas');
-  buffer.width = 256;
-  buffer.height = 240;
-
-  backgrounds.forEach(background => {
-    drawBackground(background, buffer.getContext('2d'), sprites); //selects the tiles in 1-1.json via the arrays and displays them
-  });
-  return function drawBackgroundLayer(context) {
-    context.drawImage(buffer, 0, 0);
-  };
-}
 
 function createSpriteLayer(sprite, pos) {
   return function drawSpriteLayer(context) {
