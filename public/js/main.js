da87d1a20f9778eff2d216d931b4dafed1818f3d
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-06-04 16:08:08 +0100

    made a small army of marios

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ const context = canvas.getContext('2d');
 
 function createSpriteLayer(sprite, pos) {
   return function drawSpriteLayer(context) {
-    for(let i = 0; i < 20; ++i) {
+    for(let i = 0; i < 20; ++i) { // adding more marios
       sprite.draw('idle', context, pos.x + i * 16, pos.y);
     }
   }
