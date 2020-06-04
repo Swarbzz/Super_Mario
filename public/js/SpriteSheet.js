8cd384b2846bec93dfab0e2ff1316130f5adb06a
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-06-04 14:20:37 +0100

    index file displays the sky and the ground

--- public/js/SpriteSheet.js
+++ public/js/SpriteSheet.js
@@ -1,28 +1,31 @@
 export default class SpriteSheet {
-    constructor(image, width, height){
-      this.image = image;
-      this.width = width;
-      this.height = height;
-      this.tiles = new Map();
-    }
+  constructor(image, width, height){
+    this.image = image;
+    this.width = width;
+    this.height = height;
+    this.tiles = new Map();
+  }
   
-    define(name, x, y) {
-      const buffer = document.createElement('canvas');
-      buffer.width = this.width;
-      buffer.height = this.height;
-      buffer.getContext('2d').drawImage(
-        this.image,
-        x * this.width,
-        y * this.height,
-        this.width,
-        this.height,
-        0, 0,
-        this.width,
-        this.height);
-      this.tiles.set(name, buffer);
-    }
-    draw(name, context, x, y) {
-      const buffer = this.tiles.get(name);
-      context.drawImage(buffer, x, y); //drawImage is polymorphic, able to take many arguments to get different results.
-    }
+  define(name, x, y) {
+    const buffer = document.createElement('canvas');
+    buffer.width = this.width;
+    buffer.height = this.height;
+    buffer.getContext('2d').drawImage(
+      this.image,
+      x * this.width,
+      y * this.height,
+      this.width,
+      this.height,
+      0, 0,
+      this.width,
+      this.height);
+    this.tiles.set(name, buffer);
+  }
+  draw(name, context, x, y) {
+    const buffer = this.tiles.get(name);
+    context.drawImage(buffer, x, y); //drawImage is polymorphic, able to take many arguments to get different results.
+  }
+  drawTile(name, context, x, y) {
+    this.draw(name, context, x* this.width, y * this.height); //multipled by 16 because the draw function doesn't include tile sizes
+  }
   }
--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,18 @@ context.fillRect(0, 0, 50, 50);
 loadImage('/image/tiles.png')
 .then(image => {
   const sprites = new SpriteSheet(image, 16, 16); //this line determines which tile to select
-  sprites.define('ground', 0, 0);
-  sprites.draw('ground',context, 45 ,62); //this line determines where it should be displayed on the page
+  sprites.define('ground', 0, 0); // selects the ground tile 
+  sprites.define('sky', 3, 23); // selects the sky tile
+
+  for (let x = 0; x < 25; ++x) { //width of the sky
+    for (let y = 0; y < 14; ++ y) { // length of the sky
+      sprites.drawTile('sky',context, x, y); //this line determines where it should be displayed on the page
+    }
+  }
+
+  for (let x = 0; x < 25; ++x) { //width of the ground
+    for (let y = 12; y < 14; ++ y) { // length of the ground
+      sprites.drawTile('ground',context, x, y); //this line determines where it should be displayed on the page
+    }
+  }
 });
