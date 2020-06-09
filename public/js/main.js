52dde408ef39e9823e3d6892abed80fa2b905d08
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-06-09 16:27:31 +0100

    moved code to seperate modules

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 import Compositor from './compsitor.js';
 import {loadLevel} from './Loaders.js';
-import {loadBackgroundSprites, loadMarioSprite} from './sprites.js';
+import {createMario} from './entities.js';
+import {loadBackgroundSprites} from './sprites.js';
 import {createBackgroundLayer} from './layers.js';
 
 const canvas = document.getElementById('screen');
@@ -12,47 +13,17 @@ function createSpriteLayer(entity) {
     }
 }
 
-class Vec2 {
-  constructor(x, y) {
-    this.set(x, y)
-  }
-  set(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
-class Entity {
-  constructor() {
-    this.pos = new Vec2(0, 0);
-    this.vel = new Vec2(0, 0);
-  }
-}
-
 Promise.all([ //allows sprites and level to load at the same time instead of one after another. 
-  loadMarioSprite(),
+  createMario(),
   loadBackgroundSprites(),
   loadLevel('1-1'),
 ])
-.then(([marioSprite, backgroundSprites, level]) => {
+.then(([mario, backgroundSprites, level]) => {
   const comp = new Compositor();
   const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
   comp.layers.push(backgroundLayer);
 
   const gravity = 0.5;
-
-  const mario = new Entity();
-  mario.pos.set(64, 180);
-  mario.vel.set(2, -10);
-
-  mario.draw = function drawMarion(context) {
-    marioSprite.draw('idle', context, this.pos.x, this.pos.y);
-  }
-
-  mario.update = function updateMario() {
-    this.pos.x += this.vel.x;
-    this.pos.y += this.vel.y;
-  }
 
   const spriteLayer = createSpriteLayer(mario);
   comp.layers.push(spriteLayer);
