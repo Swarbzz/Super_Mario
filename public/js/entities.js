4c20a82533cb1e22ff851207a6353c9f1fc0ec15
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-06-15 15:53:50 +0100

    moved velocity into its own class with traits

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,18 +1,27 @@
-import Entity from './entity.js'
+import Entity, {Trait} from './entity.js'
 import {loadMarioSprite} from './sprites.js';
+
+class Velocity extends Trait{
+  constructor() {
+    super('velocity');
+  }
+  
+  update(entity,deltaTime) {
+    entity.pos.x += entity.vel.x * deltaTime;
+    entity.pos.y += entity.vel.y * deltaTime;
+  }
+}
+
 
 export function createMario() {
   return loadMarioSprite()
   .then(sprite => {
     const mario = new Entity();
 
+    mario.addTrait(new Velocity());
+
     mario.draw = function drawMarion(context) {
       sprite.draw('idle', context, this.pos.x, this.pos.y);
-    }
-
-    mario.update = function updateMario(deltaTime) {
-      this.pos.x += this.vel.x * deltaTime;
-      this.pos.y += this.vel.y * deltaTime;
     }
     return mario;
   });
