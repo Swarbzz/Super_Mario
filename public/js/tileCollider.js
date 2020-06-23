c2aaf3fe183868aa3e1a71d4e9d0958d2eefe971
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-06-23 15:36:21 +0100

    the blue squares now finds the squares around it

--- public/js/tileCollider.js
+++ public/js/tileCollider.js
@@ -6,25 +6,27 @@ export default class TileCollider {
   }
 
   checkY(entity) {
-    const match = this.tiles.matchByPosition(entity.pos.x, entity.pos.y);
-    if (!match) {
-      return;
+    const matches = this.tiles.searchByRange(
+      entity.pos.x, entity.pos.x + entity.size.x,
+      entity.pos.y, entity.pos.y + entity.size.y
+      );
+      matches.forEach (match => {
+        if (match.tile.name !== 'ground') {
+          return;
+        }
+        if (entity.vel.y > 0) {
+          if (entity.pos.y > match.y1) {
+            entity.pos.y = match.y1; // y1 is a number calculated by the TileResolver
+            entity.vel.y = 0; // mario will stop and hit the floor
+          }
+        } else if (entity.vel.y < 0) {
+          if (entity.pos.y < match.y2) {
+            entity.pos.y = match.y2; // y2 is a number calculated by the TileResolver
+            entity.vel.y = 0; // mario will bump his head on the ceiling 
+          }
+        }
+      });
     }
-    if (match.tile.name !== 'ground') {
-      return;
-    }
-    if (entity.vel.y > 0) {
-      if (entity.pos.y > match.y1) {
-        entity.pos.y = match.y1; // y1 is a number calculated by the TileResolver
-        entity.vel.y = 0; // mario will stop and hit the floor
-      }
-    } else if (entity.vel.y < 0) {
-      if (entity.pos.y < match.y2) {
-        entity.pos.y = match.y2; // y2 is a number calculated by the TileResolver
-        entity.vel.y = 0; // mario will bump his head on the ceiling 
-      }
-    }
-  }
 
   test(entity) {
     this.checkY(entity)
