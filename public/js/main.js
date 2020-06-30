071ddbc5289b1e27dd2fdc8ddea16d703358bdce
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-06-30 14:50:43 +0100

    Can now scroll right with the right click and drag mouse button

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ import {loadLevel} from './Loaders.js';
 import {createMario} from './entities.js';
 import {setUpKeyboard} from './input.js';
 import {createCollisionLayer} from './layers.js'
+import {setupMouseControl} from './debug.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -27,6 +28,8 @@ Promise.all([ //allows sprites and level to load at the same time instead of one
   const input = setUpKeyboard(mario);
 
   input.listenTo(window);
+
+  setupMouseControl(canvas, mario, camera);
 
   const timer = new Timer(1/60);
   timer.update = function update(deltaTime) {
