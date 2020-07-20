24ed6758615285f7cb947b669e272227e3735ca5
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-07-20 18:41:03 +0100

    omg i cannot find this bug

--- public/js/layers.js
+++ public/js/layers.js
@@ -27,7 +27,7 @@ export function createBackgroundLayer(level, sprites) {
       const col = tiles.grid[x];
       if (col) {
         col.forEach((tile, y) => {
-          if (tile.name === 'chance') {
+          if (sprites.animations,has(tile.name)) {
             sprites.drawAnim(tile.name, context, x - startIndex, y, level.totalTime);
           } else {
           sprites.drawTile(tile.name, context, x - startIndex, y);
