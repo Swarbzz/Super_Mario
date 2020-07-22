85b6bb15f5772bc43af05a09429f1b84b35cc0b8
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-07-22 19:30:59 +0100

    mario now accelerates to a top speed

--- public/js/traits/go.js
+++ public/js/traits/go.js
@@ -4,21 +4,24 @@ export default class Go extends Trait {
   constructor() {
     super('go');
     this.direction = 0; 
-    this.speed = 7000; 
+    this.acceleration = 400; 
+    this.dragFactor = 1/5000
 
     this.distance = 0;
     this.heading = 1; //where mario is facing
   }
     
   update(entity, deltaTime) {
-      entity.vel.x = this.speed * this.direction * deltaTime;
-
-      if (this.direction) {
+    const absX =  Math.abs(entity.vel.x)
+        if (this.direction) {
+        entity.vel.x += this.acceleration* deltaTime  * this.direction ;
         this.heading = this.direction;
-        this.distance += Math.abs(entity.vel.x) * deltaTime;
+        this.distance += absX * deltaTime;
       } else {
         this.distance = 0;
       }
+      const drag = this.dragFactor * entity.vel.x * absX
+      entity.vel.x -= drag;
   }
 }
   
