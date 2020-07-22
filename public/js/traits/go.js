c0a62668334703957c1b3d0a548fafc35113e56e
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-07-22 19:34:30 +0100

    mario doesn slide on ice anymore

--- public/js/traits/go.js
+++ public/js/traits/go.js
@@ -5,6 +5,7 @@ export default class Go extends Trait {
     super('go');
     this.direction = 0; 
     this.acceleration = 400; 
+    this.deceleration = 300;
     this.dragFactor = 1/5000
 
     this.distance = 0;
@@ -13,10 +14,13 @@ export default class Go extends Trait {
     
   update(entity, deltaTime) {
     const absX =  Math.abs(entity.vel.x)
-        if (this.direction) {
+      if (this.direction != 0) {
         entity.vel.x += this.acceleration* deltaTime  * this.direction ;
         this.heading = this.direction;
         this.distance += absX * deltaTime;
+      } else if (entity.vel.x != 0){
+        const decel = Math.min(absX, this.deceleration * deltaTime)
+        entity.vel.x += entity.vel.x > 0 ? -decel : decel;
       } else {
         this.distance = 0;
       }
