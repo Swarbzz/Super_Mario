ae31987215b40e656b9e3a8d002e98f154278817
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-07-22 19:20:58 +0100

    the camera now follows mario

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,6 @@ import Camera from './camera.js';
 import {loadLevel} from './Loaders.js';
 import {createMario} from './entities.js';
 import {setUpKeyboard} from './input.js';
-import {createCollisionLayer, createCameraLayer} from './layers.js'
-import {setupMouseControl} from './debug.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -20,20 +18,17 @@ Promise.all([ //allows sprites and level to load at the same time instead of one
   mario.pos.set(64, 64); // change his position
 
   level.entities.add(mario);
-
-  // createCollisionLayer(level); 
-
-  /* level.comp.layers.push(createCollisionLayer(level),
-  createCameraLayer(camera)); */ // debugging
-
   const input = setUpKeyboard(mario);
   input.listenTo(window);
-
-  setupMouseControl(canvas, mario, camera);
 
   const timer = new Timer(1/60);
   timer.update = function update(deltaTime) {
     level.update(deltaTime)
+
+    if (mario.pos.x > 100) {
+      camera.pos.x = mario.pos.x - 100
+    }
+
     level.comp.draw(context, camera)
   }
   timer.start();
