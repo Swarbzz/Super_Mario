01793bab698deb94e89e72f5f7dc6d782a900a26
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-10-28 12:04:18 +0000

    Mario no longer goes at light speed

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -5,7 +5,8 @@ export default class Go extends Trait {
         super('go');
 
         this.dir = 0;
-        this.speed = 6000;
+        this.acceleration = 400;
+        this.dragFactor = 1/5000; // so Mario doesn't go at light speed
 
 
         this.distance = 0;
@@ -13,13 +14,16 @@ export default class Go extends Trait {
     }
 
     update(entity, deltaTime) {
-        entity.vel.x = this.speed * this.dir * deltaTime;
 
         if (this.dir) {
+            entity.vel.x += this.acceleration * this.dir * deltaTime;
             this.heading = this.dir;
             this.distance += Math.abs(entity.vel.x) * deltaTime;
         } else {
             this.distance = 0;
         }
+
+        const drag = this.dragFactor * entity.vel.x * Math.abs(entity.vel.x);
+        entity.vel.x -= drag;
     }
 }
