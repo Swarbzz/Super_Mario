9530f95c30da3627e3661fe40b64a24bb5af3cc1
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-11-19 14:27:27 +0000

    The Koopa now fits appropriately, i.e is the same height as the goomba

--- public/js/entities/Mario.js
+++ public/js/entities/Mario.js
@@ -39,6 +39,7 @@ function createMarioFactory(sprite) {
     return function createMario() {
         const mario = new Entity();
         mario.size.set(14, 16);
+        mario.offset.set(24, 32);
 
         mario.addTrait(new Go());
         mario.addTrait(new Jump());
